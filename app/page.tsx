ccc03014d3c80dc2ccc64fcae755211be07c1771
Eduardo Brito <eduardo370@outlook.pt>
2025-07-11 11:18:27 +0100

    atualização no upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import FileUploader from "@/components/fileUploader";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-5 ">
       <p className="text-3xl text-black">Formulário de Upload</p>
-      <FileUploader />
+      <Link href={'/avatar/upload'}>PAGINA DE UPLOAD</Link>
     </div>
   );
 }
